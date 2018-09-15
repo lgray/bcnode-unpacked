a69beea64b89b660124e34b63dcc80d88ccc6c46
lgray <lindsey.gray@gmail.com>
2018-09-15 11:23:50 -0500

    works on a gpu now

--- gpu_dev/bc_miner.cu
+++ gpu_dev/bc_miner.cu
@@ -1,26 +1,141 @@
+#include "blake2.h"
 #include "blake2b.cu"
 #include "cos_dist.cu"
+#include <curand_kernel.h>
+#include "stdio.h"
 
-#include <string>
+static const unsigned N_MINER_THREADS_PER_BLOCK = 64;
 
-__device__ __host__ __forceinline__ unsigned long long
-one_unit_work(uint8_t work[BLAKE2B_OUTBYTES],
-              uint8_t compare[1024],
-              size_t compare_size) {
-  blake2b_state s;
-  uint8_t eph_hash[BLAKE2B_OUTBYTES];
-  
-  blake2b_init_cu(&s,BLAKE2B_OUTBYTES);  
-  blake2b_update_cu(&s,compare,compare_size);
-  blake2b_final_cu(&s,eph_hash,BLAKE2B_OUTBYTES);
-  
-  return cosine_distance_cu(work,eph_hash);
+__constant__ uint8_t miner_key_[BLAKE2B_OUTBYTES];
+__constant__ uint8_t merkle_root_[BLAKE2B_OUTBYTES];
+__constant__ uint8_t received_work_[BLAKE2B_OUTBYTES];
+__constant__ uint8_t time_stamp_[BLAKE2B_OUTBYTES];
+__constant__ size_t miner_key_size_;
+__constant__ size_t time_stamp_size_;
+__constant__ unsigned long long the_difficulty_;
+
+
+struct bc_mining_data {
+  static const size_t INLENGTH = 2048;
+  uint8_t result[BLAKE2B_OUTBYTES], data_in[INLENGTH];
+  uint64_t distance;
+  uint32_t nonce;
+  size_t data_size;
+  bool over_difficulty;
+};
+
+__global__ void setup_rand(curandState *state)
+{
+  int id = threadIdx.x + blockIdx.x * N_MINER_THREADS_PER_BLOCK;
+  /* Each thread gets same seed, a different sequence 
+     number, no offset */
+  curand_init(1234^(uint64_t)state, id, 0, &state[id]);
 }
 
-__device__ __host__ 
-void prepare_comparison(const std::string&) {
+//__device__ __host__ __forceinline__ 
+__global__
+void one_unit_work(bc_mining_data* mining_info) {
+
+  uint8_t received_work[BLAKE2B_OUTBYTES];
+  memcpy(received_work,received_work_,BLAKE2B_OUTBYTES);
+
+  int id = threadIdx.x + blockIdx.x * N_MINER_THREADS_PER_BLOCK;
+  
+  bc_mining_data mine_this;
+  memcpy(&mine_this,&mining_info[id],sizeof(bc_mining_data));
+
+  blake2b_state s;
+  blake2b_init_cu(&s,BLAKE2B_OUTBYTES);  
+  blake2b_update_cu(&s,mine_this.data_in,mine_this.data_size);
+  blake2b_final_cu(&s,mine_this.result,BLAKE2B_OUTBYTES);
+  
+  mine_this.distance = cosine_distance_cu(received_work,mine_this.result);
+
+  memcpy(&mining_info[id],&mine_this,sizeof(bc_mining_data));
+}
+
+__global__
+void prepare_work(curandState *state, bc_mining_data* mining_info) {
+
+  size_t miner_key_size,tstamp_size;
+  uint8_t tstamp[BLAKE2B_OUTBYTES],miner_key[BLAKE2B_OUTBYTES],merkle_root[BLAKE2B_OUTBYTES];
+  memcpy(&tstamp_size,&time_stamp_size_,sizeof(size_t));  
+  memcpy(tstamp,time_stamp_,time_stamp_size_);
+  memcpy(&miner_key_size,&miner_key_size_,sizeof(size_t));  
+  memcpy(miner_key,miner_key_,miner_key_size);
+  memcpy(merkle_root,merkle_root_,BLAKE2B_OUTBYTES);
+
+  static uint16_t num_to_code[16] = {48,49,50,51,52,53,54,55,56,57,97,98,99,100,101,102};
+  static uint64_t powers_of_ten[11] = { 1,
+					10,
+					100,
+					1000,
+					10000,
+					100000,
+					1000000,
+					10000000,
+					100000000,
+					1000000000,
+					10000000000};
+					
+  
+  int id = threadIdx.x + blockIdx.x * N_MINER_THREADS_PER_BLOCK;
+
+  
+  
+  bc_mining_data mine_this;
+  memcpy(&mine_this,&mining_info[id],sizeof(bc_mining_data));  
+  curandState localState = state[id];
+  uint8_t nonce_string[12]; // ten bytes and a null character max;
+  uint8_t nonce_hash[BLAKE2B_OUTBYTES];
+  memset(nonce_string,0,12);
+  
+  //uint32_t temp = curand(&localState);
+  mine_this.nonce = curand(&localState);
+  
+  // convert nonce
+  nonce_string[0] = '0'; // take care of base case
+  uint32_t length = 0;
+  while( mine_this.nonce >= powers_of_ten[length] ) { ++length; }
+  for( uint32_t i = 0; i < length; ++i ){    
+    nonce_string[length-i-1] = num_to_code[(mine_this.nonce/powers_of_ten[i])%10];
+  }
+
+  // create the nonce hash
+  blake2b_state ns;
+  blake2b_init_cu(&ns,BLAKE2B_OUTBYTES);  
+  blake2b_update_cu(&ns,nonce_string,length);
+  blake2b_final_cu(&ns,nonce_hash,BLAKE2B_OUTBYTES);
+
+  // convert nonce in place to string codes and "blake2bl" form
+  for( unsigned i = 32; i < BLAKE2B_OUTBYTES; ++i ) {
+    uint8_t byte = nonce_hash[i];
+    nonce_hash[2*(i-32)] = num_to_code[byte>>4];
+    nonce_hash[2*(i-32)+1] = num_to_code[byte&0xf];
+  }
+  
+  // now we put everything into the data_in string in stringified hex form
+  uint32_t index = 0;
+  memcpy(mine_this.data_in,miner_key,miner_key_size);
+  index += miner_key_size;  
+  memcpy(mine_this.data_in+index,merkle_root,BLAKE2B_OUTBYTES);
+  index += BLAKE2B_OUTBYTES;
+  memcpy(mine_this.data_in+index,nonce_hash,BLAKE2B_OUTBYTES);
+  index += BLAKE2B_OUTBYTES;
+  memcpy(mine_this.data_in+index,tstamp,tstamp_size);
+  index += tstamp_size;
+  mine_this.data_size = index;
+
+  //copy the local work back to the gpu memory
+  memcpy(&mining_info[id],&mine_this,sizeof(bc_mining_data));
+
+  state[id] = localState;
+}
+
+__host__ void do_mining(uint8_t work[BLAKE2B_OUTBYTES]) {
   
 }
 
 __device__ __host__ void launch_mining(uint8_t work[BLAKE2B_OUTBYTES]) {
+  
 }
--- gpu_dev/main.cu
+++ gpu_dev/main.cu
@@ -2,6 +2,9 @@
 #include "bc_miner.cu"
 
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
 
 struct BCHash {
    blake2b_state state;
@@ -24,20 +27,44 @@ INFO	 mining.primitives compr: 2b08d9146a6ce1f02db88203bdc653cd0b5e33ec945f391f8
 INFO	 mining.primitives testr: 204933315567342 undefined
 */
 
+int mypow(int base, int exp) {
+  int result = 1;
+  while( exp-- ) { result *= base; }
+  return result;
+}
+
+bool sort_by_distance(const bc_mining_data& i1, 
+		      const bc_mining_data& i2) { 
+  return i1.distance > i2.distance; 
+} 
+
 int main(int argc, char **argv) {
     BCHash cpu = BCHash();
     BCHash gpu = BCHash();
 
-    std::string work("7296c034e95304ee2a69c2a61e6287a058448d08d723222d19658c0364dd247c");
+    std::string work("0edd781347cfc9c3ff49fdc423c7f1a3deae6501e5cef6b99c45c8901f763320");
     std::string mhash("0xf34fa87db39d15471bebe997860dcd49fc259318");
-    std::string merkl("108459d41ce2399e13992528ea1bd9940fac9df181e29c2b61caeaad55f6532a");
-    std::string nhash    ("1af153f4cf971b61cc867b760bebfbc98a8c216cd69d6bb5dbb63aaed9db1fc3");
-    std::string times("1536810114");
+    std::string merkl("7aff5341ec1a1caa51c74c162c7f2a3946fe28f23b6e630de995f74d5767f865");
+    unsigned int thenonce = 2060688607;
+    uint8_t nonce_string[12]; // ten bytes and a null character max;
+    memset(nonce_string,0,12);
+    // convert nonce
+    static uint16_t num_to_code[16] = {48,49,50,51,52,53,54,55,56,57};
+    nonce_string[0] = '0'; // take care of base case
+    uint32_t length = 0;
+    while( thenonce >= std::pow(10,length) ) { ++length; }
+    std::cout << "the length: " << length << std::endl;
+    for( uint32_t i = 0; i < length; ++i ){
+      nonce_string[length-i-1] = num_to_code[(thenonce/mypow(10,i))%10];
+    }
+    std::cout << thenonce << ' ' << nonce_string << std::endl;    
+    std::string nhash("cb5d17fe5c27f7b7426002eb665142d00190553b9d945a936eed3ffd23cdde71");
+    std::string times("1536783719");
 
     std::string the_thing = mhash + merkl + nhash + times;
 
-    std::string result_bc("2b08d9146a6ce1f02db88203bdc653cd0b5e33ec945f391f82dbe3d417fb586e");
-    
+    std::string result_bc("c0d42acc9793a81096411b74b78fe9a12645737c57ee1544fb35d5fa6f09503e");
+     
     std::cout << the_thing << std::endl;
     std::cout << result_bc << std::endl;
     
@@ -58,12 +85,28 @@ int main(int argc, char **argv) {
       work_char[i/2] += strtol(temp,NULL,16)<<(4*((i+1)%2));
     }
     //memcpy(work_char,work.c_str(),work.size()*sizeof(uint8_t)/2);
-    
-    blake2b_init(&cpu.state,BLAKE2B_OUTBYTES);
-    blake2b_update(&cpu.state,empty_cpu,the_thing.size());
-    blake2b_final(&cpu.state,hash_cpu,BLAKE2B_OUTBYTES);
 
-    std::cout << BLAKE2B_OUTBYTES << std::endl;
+    const unsigned long long hash_tries = 1 << 18;
+
+    clock_t begin = clock();
+
+    bc_mining_data* testhost = NULL;
+    testhost = (bc_mining_data*)malloc(hash_tries*sizeof(bc_mining_data));
+    memset(testhost,0,hash_tries*sizeof(bc_mining_data));
+
+    for(unsigned long long i = 0; i < hash_tries; ++i ) {    
+      blake2b_init(&cpu.state,BLAKE2B_OUTBYTES);
+      blake2b_update(&cpu.state,empty_cpu,the_thing.size());
+      blake2b_final(&cpu.state,hash_cpu,BLAKE2B_OUTBYTES);
+
+    }
+
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin);
+    
+    std::cout<< "cpu took clocks: " << elapsed_secs << std::endl;
+
+    std::cout << BLAKE2B_OUTBYTES << ' ' << work_char << std::endl;
 
     std::cout << "input work: 0x";
     for( unsigned i = 0; i < BLAKE2B_OUTBYTES ; ++i ) {
@@ -80,19 +123,74 @@ int main(int argc, char **argv) {
     double dist_cpu = cosine_distance_cu(work_char,hash_cpu);
     std::cout << "cpu distance is: " << (unsigned long long)(dist_cpu) << std::endl;
 
-    blake2b_init_cu(&gpu.state,BLAKE2B_OUTBYTES);
-    blake2b_update_cu(&gpu.state,empty_gpu,the_thing.size());
-    blake2b_final_cu(&gpu.state,hash_gpu,BLAKE2B_OUTBYTES);
-    std::cout << "gpu: " << uint32_t(gpu.state.buflen) << " trial = 0x" << std::hex;
+    // now let's do it on the GPU for real
+    size_t stash_size = mhash.length();
+    size_t tstamp_size = times.length();
+    cudaMemcpyToSymbol(time_stamp_size_, &tstamp_size, sizeof(size_t));
+    cudaMemcpyToSymbol(time_stamp_, times.c_str(), times.length());
+    cudaMemcpyToSymbol(miner_key_size_, &stash_size, sizeof(size_t));
+    cudaMemcpyToSymbol(miner_key_, mhash.c_str(), mhash.length());
+    cudaMemcpyToSymbol(received_work_, work_char, BLAKE2B_OUTBYTES);
+    cudaMemcpyToSymbol(merkle_root_,merkl.c_str(), BLAKE2B_OUTBYTES);
+    bc_mining_data* testdev = NULL;
+    cudaMalloc(&testdev,hash_tries*sizeof(bc_mining_data));     
+    cudaMemset(testdev,0,hash_tries*sizeof(bc_mining_data));
+        
+    //random numbers
+    curandState *devStates;
+    cudaMalloc((void **)&devStates, hash_tries * 1 * sizeof(curandState));
+
+    //setup test information
+    for(unsigned long long i = 0; i < hash_tries; ++i ) {
+      cudaMemcpy(&testdev[i].nonce,&thenonce,sizeof(uint32_t),cudaMemcpyHostToDevice);
+    }
+    
+    clock_t begin_gpu = clock();
+
+    setup_rand<<<hash_tries/N_MINER_THREADS_PER_BLOCK,N_MINER_THREADS_PER_BLOCK>>>(devStates);
+    prepare_work<<<hash_tries/N_MINER_THREADS_PER_BLOCK,N_MINER_THREADS_PER_BLOCK>>>(devStates,testdev);
+    one_unit_work<<<hash_tries/N_MINER_THREADS_PER_BLOCK,N_MINER_THREADS_PER_BLOCK>>>(testdev);
+    cudaDeviceSynchronize();
+
+    clock_t end_gpu = clock();
+    double elapsed_secs_gpu = double(end_gpu - begin_gpu);
+    
+    std::cout<< "gpu took clocks: " << elapsed_secs_gpu << std::endl;
+
+    cudaMemcpy(testhost,testdev,hash_tries*sizeof(bc_mining_data),cudaMemcpyDeviceToHost);
+    
+    std::cout << "gpu: " << testhost[0].data_size << " trial = 0x" << std::hex;
     // output "blake2bl"
-    for( unsigned i = 32; i < BLAKE2B_OUTBYTES ; ++i ) {
-    	 std::cout << std::hex << (unsigned)(hash_gpu[i]>>4) << (unsigned)(hash_gpu[i]&0xf);
+    for( unsigned i = 32; i < BLAKE2B_OUTBYTES; ++i ) {
+    	 std::cout << std::hex << (unsigned)(testhost[0].result[i]>>4) << (unsigned)(testhost[0].result[i]&0xf);
     }
     std::cout << std::dec << std::endl;
-    unsigned long long dist_gpu = one_unit_work(work_char,empty_gpu,the_thing.size());
-    std::cout << "gpu distance is: " << (dist_gpu) << std::endl;
+    std::cout << "gpu distance is: " << testhost[1].distance << std::endl;
 
+    //unsigned long long dist_gpu = cosine_distance_cu(work_char,hash_gpu);//one_unit_work(work_char,empty_gpu,the_thing.size());
     
+    std::sort(  testhost, 
+                testhost + hash_tries,
+                sort_by_distance );
+    
+    for( unsigned i = 32; i < BLAKE2B_OUTBYTES; ++i ) {
+    	 std::cout << std::hex << (unsigned)(testhost[0].result[i]>>4) << (unsigned)(testhost[0].result[i]&0xf);
+    }
+    std::cout << std::dec << std::endl;
+    std::cout << "gpu distance is: " << testhost[0].distance << std::endl;
+
+    for( unsigned i = 32; i < BLAKE2B_OUTBYTES; ++i ) {
+      std::cout << std::hex << (unsigned)(testhost[hash_tries-1].result[i]>>4) << (unsigned)(testhost[hash_tries-1].result[i]&0xf);
+    }
+    std::cout << std::dec << std::endl;
+    std::cout << "gpu distance is: " << testhost[hash_tries-1].distance << std::endl;
+    
+
+    cudaFree(testdev);
+    free(testhost);
     
     return 0;
 }
+
+//301973303693442
+//277301613100604
